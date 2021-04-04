5a085cfed05500c6e6bf69b5326b5c8815a73ad7
stoa <stoa@stoa.com>
2021-04-04 21:43:13 +0200

    second commit

--- script.js
+++ script.js
@@ -1,24 +1,25 @@
 let logos = new Map();
-logos.set(524, "psg.svg");
-logos.set(521, "losc.svg");
-logos.set(523, "lyon.svg");
-logos.set(548, "monaco.svg");
-logos.set(546, "lens.svg");
-logos.set(516, "om.svg");
-logos.set(529, "rennes.svg");
-logos.set(518, "montpellier.svg");
-logos.set(545, "metz.svg");
-logos.set(532, "angers.svg");
-logos.set(522, "nice.svg");
-logos.set(547, "reims.svg");
-logos.set(526, "bordeaux.svg");
-logos.set(512, "brest.svg");
-logos.set(576, "strasbourg.svg");
-logos.set(527, "saintetienne.svg");
-logos.set(525, "lorient.svg");
-logos.set(556, "nimes.svg");
-logos.set(543, "nantes.svg");
-logos.set(528, "dijon.svg");
+logos.set(524, "logos/psg.svg");
+logos.set(521, "logos/losc.svg");
+logos.set(523, "logos/lyon.svg");
+logos.set(548, "logos/monaco.svg");
+logos.set(546, "logos/lens.svg");
+logos.set(516, "logos/om.svg");
+logos.set(529, "logos/rennes.svg");
+logos.set(518, "logos/montpellier.svg");
+logos.set(545, "logos/metz.svg");
+logos.set(532, "logos/angers.svg");
+logos.set(522, "logos/nice.svg");
+logos.set(547, "logos/reims.svg");
+logos.set(526, "logos/bordeaux.svg");
+logos.set(512, "logos/brest.svg");
+logos.set(576, "logos/strasbourg.svg");
+logos.set(527, "logos/saintetienne.svg");
+logos.set(525, "logos/lorient.svg");
+logos.set(556, "logos/nimes.svg");
+logos.set(543, "logos/nantes.svg");
+logos.set(528, "logos/dijon.svg");
+
 
 fetch("http://api.football-data.org/v2/competitions/FL1/standings", {
   method: "GET",
@@ -28,8 +29,29 @@ fetch("http://api.football-data.org/v2/competitions/FL1/standings", {
 })
   .then((res) => res.json())
   .then((data) => {
+    let names = new Map();
+    names.set(524, "PSG");
+    names.set(521, "LILLE");
+    names.set(523, "OL");
+    names.set(548, "AS MONACO");
+    names.set(546, "LENS");
+    names.set(516, "OM");
+    names.set(529, "RENNES");
+    names.set(518, "MONTPELLIER");
+    names.set(545, "METZ");
+    names.set(532, "ANGERS");
+    names.set(522, "OGC NICE");
+    names.set(547, "REIMS");
+    names.set(526, "BORDEAUX");
+    names.set(512, "BREST");
+    names.set(576, "STRASBOURG");
+    names.set(527, "ASSE");
+    names.set(525, "LORIENT");
+    names.set(556, "NIMES");
+    names.set(543, "NANTES");
+    names.set(528, "DIJON");
+    
     const league = data.standings[0].table;
-    console.log(league);
     league.forEach((element) => {
       const logoUrl = element.team.crestUrl;
       const domLeague = document.querySelector(".first-slide");
@@ -37,7 +59,13 @@ fetch("http://api.football-data.org/v2/competitions/FL1/standings", {
       image.setAttribute("src", logos.get(element.team.id));
       image.setAttribute("id", "myImg");
       image.classList.add("image");
-      domLeague.append(image);
+      const teamContainer = document.createElement('article');
+      const teamName = document.createElement('p');
+      teamName.append(names.get(element.team.id));
+      teamContainer.append(image);
+      teamContainer.append(teamName);
+      teamContainer.classList.add('team-container')
+      domLeague.append(teamContainer);
     });
   })
   .catch((err) => {
@@ -52,6 +80,28 @@ fetch("http://api.football-data.org/v2/competitions/PD/standings", {
 })
   .then((res) => res.json())
   .then((data) => {
+    let names = new Map();
+    names.set(78, "ATLETICO");
+    names.set(86, "REAL");
+    names.set(81, "BARCELONE");
+    names.set(559, "SEVILLE");
+    names.set(94, "VILLAREAL");
+    names.set(92, "SOCIEDAD");
+    names.set(90, "BETIS");
+    names.set(83, "GRENADE");
+    names.set(77, "BILBAO");
+    names.set(88, "LEVANTE");
+    names.set(558, "CELTA VIGO");
+    names.set(95, "VALENCE");
+    names.set(79, "OSASUNA");
+    names.set(82, "GETAFE");
+    names.set(264, "CADIX");
+    names.set(250, "VALLADOLID");
+    names.set(285, "ELCHE");
+    names.set(299, "HUESCA");
+    names.set(278, "EIBAR");
+    names.set(263, "ALAVES");
+
     const league = data.standings[0].table;
     league.forEach((element) => {
       const logoUrl = element.team.crestUrl;
@@ -59,7 +109,13 @@ fetch("http://api.football-data.org/v2/competitions/PD/standings", {
       const image = document.createElement("img");
       image.setAttribute("src", logoUrl);
       image.classList.add("image");
-      domLeague.append(image);
+      const teamContainer = document.createElement('article');
+      const teamName = document.createElement('p');
+      teamName.append(names.get(element.team.id));
+      teamContainer.append(image);
+      teamContainer.append(teamName);
+      teamContainer.classList.add('team-container')
+      domLeague.append(teamContainer);
     });
   })
   .catch((err) => {
@@ -74,14 +130,41 @@ fetch("http://api.football-data.org/v2/competitions/BL1/standings", {
 })
   .then((res) => res.json())
   .then((data) => {
+    let names = new Map();
+names.set(5, "BAYERN");
+names.set(721, "LEIPZIG");
+names.set(11, "WOLFSBURG");
+names.set(19, "FRANCFORT");
+names.set(4, "BvB");
+names.set(3, "LEVERKUSEN");
+names.set(10, "STUTTGART");
+names.set(18, "MONCHENGLADBACH");
+names.set(28, "BERLIN");
+names.set(17, "FRIBOURG");
+names.set(16, "AUGSBURG");
+names.set(2, "HOFFENHEIM");
+names.set(12, "WERDER");
+names.set(9, "HERTA");
+names.set(15, "MAINZ 05");
+names.set(1, "COLOGNE");
+names.set(38, "ARMINIA");
+names.set(6, "SCHALKE");
+
     const league = data.standings[0].table;
+    console.log(league);
     league.forEach((element) => {
       const logoUrl = element.team.crestUrl;
       const domLeague = document.querySelector(".third-slide");
       const image = document.createElement("img");
       image.setAttribute("src", logoUrl);
       image.classList.add("image");
-      domLeague.append(image);
+      const teamContainer = document.createElement('article');
+      const teamName = document.createElement('p');
+      teamName.append(names.get(element.team.id));
+      teamContainer.append(image);
+      teamContainer.append(teamName);
+      teamContainer.classList.add('team-container')
+      domLeague.append(teamContainer);
     });
   })
   .catch((err) => {
@@ -98,12 +181,7 @@ fetch("http://api.football-data.org/v2/competitions/SA/standings", {
   .then((data) => {
     const league = data.standings[0].table;
     league.forEach((element) => {
-      const logoUrl = element.team.crestUrl;
-      const domLeague = document.querySelector(".fourth-slide");
-      const image = document.createElement("img");
-      image.setAttribute("src", logoUrl);
-      image.classList.add("image");
-      domLeague.append(image);
+      
     });
   })
   .catch((err) => {
